2ed9cf5080f0ecdc3c44fb46916e85d14fcd1d6c
Dario Emanuel Diaz <dariodiaz91498@gmail.com>
2024-05-25 18:47:22 -0300

    bonvino

--- DSI/BonVino/Boundary/PantallaImportBodega.py
+++ DSI/BonVino/Boundary/PantallaImportBodega.py
@@ -1,12 +1,21 @@
 class PantallaImportBodega:
-  def habilitarVentana(self):
-    # metodos
-    pass
+  def __init__(self):
+    self.bodegasDisponibles = []
+    self.seleccionBodega = None
+    self.vinosImportados = []
+    self.resumenVinosImportados = None
     
-  def solicitarSeleccionDeBodega(self):
-    # metodos
+  def opImportarActualizacionVinosBodega(self):
+    pass
+  
+  def habilitarVentana(self):
+    pass
+  
+  def solicitarSeleccionarBodega(self):
+    pass
+  
+  def tomarSeleccionBodega(self):
     pass
   
   def mostrarResumenVinosImportados(self):
-    # metodos
     pass
--- DSI/BonVino/Entity/Bodega.py
+++ DSI/BonVino/Entity/Bodega.py
@@ -1,12 +1,11 @@
-class Bodega():
+class Bodega:
   def __init__(self, nombre, descripcion):
     self.nombre = nombre
     self.descripcion = descripcion
     
     def estaParaActualizarNovedadesVino(self):
-      # metodos
       pass
     
     def obtenerActualizacionVinos(self):
-      # metodos
       pass
+   
